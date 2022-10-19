fe7c43ec71b5cf31d35c2654a6be3865b848355e
theRealThomasDavidson <davidson.thomasj@gmail.com>
2022-10-19 15:14:45 -0400

    directives-start: 2022 Oct 19 15:14:45

--- code/4. Directives Deepdive/study/src/app/app.component.ts
+++ code/4. Directives Deepdive/study/src/app/app.component.ts
@@ -6,9 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // numbers = [1, 2, 3, 4, 5];
-  oddNumbers = [1, 3, 5];
-  evenNumbers = [2, 4];
+  numbers = [1, 2, 3, 4, 5];
   onlyOdd = false;
-  value = 5;
 }
--- code/4. Directives Deepdive/study/src/app/app.module.ts
+++ code/4. Directives Deepdive/study/src/app/app.module.ts
@@ -4,16 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
-import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
-import { UnlessDirective } from './unless.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BasicHighlightDirective,
-    BetterHighlightDirective,
-    UnlessDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,
