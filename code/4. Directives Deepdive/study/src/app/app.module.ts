626c0894fb442805e9a1e861619781172eed372e
theRealThomasDavidson <davidson.thomasj@gmail.com>
2022-10-19 15:14:41 -0400

    directives-final: 2022 Oct 19 15:14:41

--- code/4. Directives Deepdive/study/src/app/app.module.ts
+++ code/4. Directives Deepdive/study/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
-import { BetterHighlightDirective } from './better-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 import { UnlessDirective } from './unless.directive';
 
 @NgModule({
