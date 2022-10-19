f7feff3e36ef3da31256f5268ae39253faa75045
theRealThomasDavidson <davidson.thomasj@gmail.com>
2022-10-19 15:14:37 -0400

    2. Using the Renderer: 2022 Oct 19 15:14:37

--- code/4. Directives Deepdive/study/directives-start/src/app/app.module.ts
+++ code/4. Directives Deepdive/study/directives-start/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighlightDirective
+    BasicHighlightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule,
