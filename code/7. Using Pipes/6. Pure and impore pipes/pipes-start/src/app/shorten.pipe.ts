d736602cd22cc654675fbd774c2b59c6c651099d
Sai Kiran Challa <schalla@adobe.com>
2022-10-21 07:41:11 -0600

    Adding Pipes

---
+++ code/7. Using Pipes/6. Pure and impore pipes/pipes-start/src/app/shorten.pipe.ts
@@ -0,0 +1,12 @@
+
+@Pipe({
+    name: 'shorten'
+})
+export class ShortenPipe implements PipeTransform {
+    transform(value: any, limit: number) {
+        if (value.length > limit) {
+            return value.substring(0, limit) + '...';    
+        }
+        return value;
+    }
+}
